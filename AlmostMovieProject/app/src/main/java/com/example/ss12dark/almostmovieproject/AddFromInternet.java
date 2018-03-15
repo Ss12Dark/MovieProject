ae4b7195a8afc7bd2d641e91814809d05f321a72
Ss12Dark <36810113+Ss12Dark@users.noreply.github.com>
2018-03-15 10:55:19 +0200

    4

--- AlmostMovieProject/app/src/main/java/com/example/ss12dark/almostmovieproject/AddFromInternet.java
+++ AlmostMovieProject/app/src/main/java/com/example/ss12dark/almostmovieproject/AddFromInternet.java
@@ -21,7 +21,7 @@ public class AddFromInternet extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_from_internet);
 
         // Take UI components:
         listViewCandies = (ListView) findViewById(R.id.listView);
